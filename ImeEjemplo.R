0fdbf5fd7e32c6963a14be9eae2cb9d25d208af7
Gerardo <gerardo.lucero.c@usach.cl>
2022-04-11 10:46:46 -0400

    se sigue avanzando, se trabaja en el EP02

--- ImeEjemplo.R
+++ ImeEjemplo.R
@@ -19,10 +19,10 @@ library(magrittr)
 #setwd("C:\\Users\\glucero\\Desktop\\IME")
 
 #Carpeta PC USACH UBUNTU
-setwd("/home/usuario/Escritorio/IME")
+#setwd("/home/usuario/Escritorio/IME")
 
 #Carpeta PC Casa
-#setwd("C:\\Users\\gerar\\Desktop\\IME")
+setwd("C:\\Users\\gerar\\Desktop\\IME")
 
 
 
@@ -120,7 +120,13 @@ varianzaSueldos <- var(datosCasen[["ytot"]])
 #se calcula la desviacion estandar de los sueldos
 sdSueldos <- sd(datosCasen[["ytot"]])
 
-
+#Funcion Table, crea un vector de frecuencias para cierto parametro, columna o variable
+#basta poner el vector o dataframe y retornará los valores que salen de cada valor existente
+#para el caso del data frame se puede poner el valor de una columna cualquiera con el [[]]
+#por ejemplo para ver cuantos encuestados mujer y hombres hay se pone
+table(datosCasen[["sexo"]])
+#luego, antes de data se ocupa el as.data.frame para transformarlo en una tabla
+freq_hombre_mujer <- as.data.frame(table(datosCasen[["sexo"]]))
 ##########################################################################################################
 ############################SECTOR HISTOGRAMA###########################################################
 
@@ -137,13 +143,82 @@ media_sueldos_hombres <- median(tabla_hombres[["ytot"]])
 media_sueldos_mujeres <- median(tabla_mujeres[["ytot"]])
 
 #Se hace un grafico histograma con la relacion de sueldos de hombres y mujeres
-histograma_sueldos <- gghistogram(datosCasen ,
-                     x = "sexo",
+histograma <- gghistogram(datosCovid,
+                     x = "Metropolitana",
                      bins = 10,
                      add = "mean",
                      xlab = "Sexo",
                      ylab = "Frecuencia",
                      color = "blue",
                      fill = "blue")
+
+
 ############################################################################################################
 ########################### FIN SECTOR HISTOGRAMA ##########################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################################EJERCICIO PRACTICO 2 #################################################
+######################################################################################################
+
+#PREGUNTA GRUPO 1
+#¿Se encuestó más o menos la misma cantidad de gente en cada provincia de la RM?
+
+#se obtienen las frecuencias para cada provincia
+freq_provincia <- as.data.frame(table(datosCasen[["provincia"]]))
+
+#se cambia el nombre de las columnas del data frame obtenido
+colnames(freq_provincia) <- c("Provincia", "Frecuencia")
+
+#Se realiza el gráfico de barras con las frecuencias
+graficoBarras_provincia <- ggbarplot(freq_m, x = "Provincia", y = "Frecuencia",
+                    label = TRUE, lab.pos = "out", lab.col = "black",
+                    fill = "Provincia", palette = "jco",
+                    title = "Encuestados en la RM por provincia",
+                    subtitle = "Muestra")
+
+#lab.col = color del numero encima de la barra
+#lab = booleano que indica si se pone o no el numero con la frecuencia encima de la barra
+
+print(graficoBarras_provincia)
+
+
+#Hago lo mismo pero con la frecuencia de hombres y mujeres
+freq_hombre_mujer <- as.data.frame(table(datosCasen["sexo"]))
+colnames(freq_hombre_mujer) <- c("Sexo", "Frecuencia")
+#Se realiza el gráfico de barras con las frecuencias
+graficoBarras_sexo <- ggbarplot(freq_hombre_mujer, x = "Sexo", y = "Frecuencia",
+                                     label = TRUE, lab.pos = "out", lab.col = "black",
+                                     fill = "Sexo", palette = "jco",
+                                     title = "Encuestados en la RM por sexo",
+                                     subtitle = "Muestra")
+print(graficoBarras_sexo)
+
+
+
+
+
+
+################################ FIN EJERCICIO PRACTICO 2 ############################################
+######################################################################################################
+
+
+
+
+
